66de85ea611b35798820e91498eb188a13188196
Anubhav Rajput <72142278+Anubhav-Raj@users.noreply.github.com>
2021-05-10 17:51:01 +0530

    done

---
+++ js/toggle.js
@@ -0,0 +1,14 @@
+toggle=document.querySelector('.toggle')
+navbar=document.getElementsByTagName('nav')
+// navList=document.querySelector('.nav_list')
+// rightnav=document.querySelector('.rightnav')
+
+
+
+toggle.addEventListener('click',()=>
+{
+    // rightnav.classList.toggle('v-class-resp');
+    // navList.classList.toggle('v-class-resp');
+    // navbar.classList.toggle('h-nav-resp');
+    navbar.classList.toggle('navul');
+})
